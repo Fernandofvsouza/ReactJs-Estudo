6aee3a4638adf2bbd2f29e7216aa767893440f48
Fernando Souza <fernandofvsouza@icloud.com>
2025-09-15 16:22:31 +0100

    commit

---
+++ Estudo React/Aula1-Introdução/script.js
@@ -0,0 +1,9 @@
+/*
+O que é react?: React é uma biblioteca javascript desenvolvida pelo facebook que permite a criação de interface de usuario (ui) de forma eficiente e escalavel ou seja usamos react para criar o front-end do nosso projeto
+
+*Não é uma linguagem de programação
+
+*Usamos o javascript ou typescript para trabalhar com o react
+
+*é um padrão de desenvolvimento baseado em componentes
+*/
